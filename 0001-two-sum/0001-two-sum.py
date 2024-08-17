9cbc357341c69bb35e3d4c4b58079ebad7f90ee1
GADADHAR TEEGALA <75172431+gad69@users.noreply.github.com>
2024-08-17 18:32:55 +0530

    Time: 35 ms (86.36%), Space: 12.5 MB (71.45%) - LeetHub

--- 0001-two-sum/0001-two-sum.py
+++ 0001-two-sum/0001-two-sum.py
@@ -1,27 +1,24 @@
 class Solution(object):
     def twoSum(self, nums, target):
-        l = 0
-        r = len(nums)-1
-        arr = []
-        nums_with_indices = [(j,i) for i,j in enumerate(nums)]
-        nums_with_indices.sort()
+        dic = {}
         
-        while l<r:
-            temp_sum = nums_with_indices[l][0]+nums_with_indices[r][0]
+        for index,ele in enumerate(nums):
             
-            if temp_sum< target:
-                l = l+1
-                
-            elif temp_sum> target:
-                r = r-1
+            if target-ele not in dic:
+                dic[ele] = index
                 
             else:
-                arr.extend([nums_with_indices[l][1],nums_with_indices[r][1]])
-                break
+                return index,dic[target-ele]
+                
+            
+            
+            
+            
                 
                 
                 
-        return arr
+        
+        
                 
                 
                 
