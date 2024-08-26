c0c59fbc469462771262c014af844f7af095aa86
GADADHAR TEEGALA <75172431+gad69@users.noreply.github.com>
2024-08-27 03:32:09 +0530

    Time: 127 ms (49.45%), Space: 18 MB (58.89%) - LeetHub

---
+++ 0283-move-zeroes/0283-move-zeroes.py
@@ -0,0 +1,10 @@
+class Solution:
+    def moveZeroes(self, nums: List[int]) -> None:
+        l = 0
+        r = 0
+        while r<len(nums):
+            if nums[r]!=0:
+                nums[l],nums[r] = nums[r],nums[l]
+                l = l+1
+            
+            r = r+1
